bd98b4fbf2df61cd221611d1892a8944edc1859b
AdaltoRG <111100730+AdaltoRG@users.noreply.github.com>
2022-10-30 15:06:46 -0300

    Validando conta e senha, chamando funções de OPs

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,15 +6,26 @@ const password = document.getElementById("input-password");
 const confirmPassword = document.getElementById("input-password-confirm");
 const telaLogin = document.querySelector(".cadastro-screen");
 
-const contasCadastradas = [];
+const contasCadastradas = [
+  {
+    nome: "Adalto",
+    cpf: "12528235682",
+    cel: "31985565662",
+    password: "12345678",
+  },
+];
 
 const submitButton = document.getElementById("submit-button");
 const clearButton = document.getElementById("clear-button");
 
-
 //Declarações tela de operações
 const selectOperacao = document.getElementById("operacao");
+const telaOperacoes = document.querySelector(".operacoes-screen");
+const operarButton = document.getElementById("finalizar-button");
+const clearOpButton = document.getElementById("clear-op-button");
 const valorOp = document.getElementById("valorOp");
+const contaOp = document.getElementById("contaOp");
+const senhaOp = document.getElementById("senhaOp");
 
 //Limpando campos
 clearButton.addEventListener("click", (evento) => {
@@ -49,23 +60,8 @@ const criaConta = () => {
   });
   alert(`Conta ${contaAtual} registrada com sucesso.`);
   telaLogin.style.display = "none";
+  telaOperacoes.style.display = "flex";
 };
-
-
-//Definindo pattern campo de valor
-const args = {
-  allowNegative: false,
-  negativeSignAfter: false,
-  prefix: "R$",
-  suffix: "",
-  fixed: true,
-  fractionDigits: 2,
-  decimalSeparator: ",",
-  thousandsSeparator: ".",
-  cursor: "move",
-};
-const input = SimpleMaskMoney.setMask(valorOp, args);
-input.formatToNumber();
 
 //Desabilitando campo valor caso operação seja consulta de saldo
 selectOperacao.addEventListener("change", () => {
@@ -75,3 +71,67 @@ selectOperacao.addEventListener("change", () => {
     valorOp.disabled = false;
   }
 });
+
+//Declarações operações
+clearOpButton.addEventListener("click", (evento) => {
+  evento.preventDefault();
+  valorOp.value = "";
+  contaOp.value = "";
+  senhaOp.value = "";
+});
+
+operarButton.addEventListener("click", (evento) => {
+  evento.preventDefault();
+
+  switch (valorOp) {
+    case "SALDO":
+      break;
+    case "SAQUE":
+      break;
+    case "DEPOSITO":
+      break;
+    default:
+      alert("Operação inválida.");
+  }
+});
+
+const obterConta = (conta) => {
+  const contaCliente = contasCadastradas.find((c) => c.conta === conta);
+  return contaCliente;
+};
+
+// //Função Saque
+// const sacar = (){
+
+// }
+
+//Função Depósito
+const depositar = (conta, valor) => {
+  if (validarValor(valor)) {
+    if (validarSaldo(conta, valor)) {
+      let saldoAtual;
+      const contasAtualizadas = contasClientes.map((c) => {
+        if (c.conta === conta) {
+          saldoAtual = c.saldo - valor;
+          return { ...c, saldo: saldoAtual };
+        }
+        return c;
+      });
+
+      contasClientes = contasAtualizadas;
+
+      alert(`Saque efetuado com sucesso! Saldo atual: ${saldoAtual}`);
+    } else {
+      alert("Saldo insuficiente");
+    }
+  } else {
+    alert("Valor inválido");
+  }
+};
+
+//Função Consulta de Saldo
+const consultar = (conta) => {
+  const contaCliente = obterConta(conta);
+
+  alert(`Saldo atual: ${contaCliente.saldo}`);
+};
