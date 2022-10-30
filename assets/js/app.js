feabb8d7c0c27c171d294db4fa9d5e30d23b3a83
AdaltoRG <111100730+AdaltoRG@users.noreply.github.com>
2022-10-30 12:04:22 -0300

    criando interface visual e formulário de operações

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,22 @@
+//Declarações tela de login e conta
 const nome = document.getElementById("input-name");
 const cpf = document.getElementById("input-cpf");
 const cel = document.getElementById("input-cel");
 const password = document.getElementById("input-password");
 const confirmPassword = document.getElementById("input-password-confirm");
+const telaLogin = document.querySelector(".cadastro-screen");
 
 const contasCadastradas = [];
 
 const submitButton = document.getElementById("submit-button");
 const clearButton = document.getElementById("clear-button");
 
+
+//Declarações tela de operações
+const selectOperacao = document.getElementById("operacao");
+const valorOp = document.getElementById("valorOp");
+
+//Limpando campos
 clearButton.addEventListener("click", (evento) => {
   evento.preventDefault();
   nome.value = "";
@@ -18,6 +26,7 @@ clearButton.addEventListener("click", (evento) => {
   confirmPassword.value = "";
 });
 
+//Validando igualdade de senhas e criando conta
 submitButton.addEventListener("click", (evento) => {
   evento.preventDefault();
   if (password.value === confirmPassword.value) {
@@ -27,6 +36,7 @@ submitButton.addEventListener("click", (evento) => {
   }
 });
 
+//Funcionalidade de criação de contas
 const criaConta = () => {
   const contaAtual = Math.floor(1000 + Math.random() * 90000);
   contasCadastradas.push({
@@ -38,4 +48,30 @@ const criaConta = () => {
     saldo: 0,
   });
   alert(`Conta ${contaAtual} registrada com sucesso.`);
+  telaLogin.style.display = "none";
 };
+
+
+//Definindo pattern campo de valor
+const args = {
+  allowNegative: false,
+  negativeSignAfter: false,
+  prefix: "R$",
+  suffix: "",
+  fixed: true,
+  fractionDigits: 2,
+  decimalSeparator: ",",
+  thousandsSeparator: ".",
+  cursor: "move",
+};
+const input = SimpleMaskMoney.setMask(valorOp, args);
+input.formatToNumber();
+
+//Desabilitando campo valor caso operação seja consulta de saldo
+selectOperacao.addEventListener("change", () => {
+  if (selectOperacao.value === "SALDO") {
+    valorOp.disabled = true;
+  } else {
+    valorOp.disabled = false;
+  }
+});
